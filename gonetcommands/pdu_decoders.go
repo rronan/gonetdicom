20d6a30f393c74444b4785653f00e55c11d9d9b7
PierreBou <pierre.bouriat@outlook.fr>
2022-12-20 16:13:50 +0100

    echoscu branch

---
+++ gonetcommands/pdu_decoders.go
@@ -0,0 +1,16 @@
+package gonetcommands
+
+// type ConstraintTest interface {
+// 	a | int | string
+// }
+
+// type a struct {
+// 	qwe string
+// 	sdf int
+// }
+
+// func MyFunc[T ConstraintTest](b []byte) T {
+// 	var myint T
+
+// 	return myint
+// }
